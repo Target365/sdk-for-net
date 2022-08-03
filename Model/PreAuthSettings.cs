27f9bb291e780c787c292f513cc160a7fb4be23c
hans.olav@target365.no <hans.olav@target365.no>
2022-08-03 13:44:24 +0200

    Annotated PreAuthSettings with DataContract attribute etc.

--- Model/PreAuthSettings.cs
+++ Model/PreAuthSettings.cs
@@ -1,48 +1,59 @@
-﻿namespace Target365.Sdk
+﻿using System.Runtime.Serialization;
+
+namespace Target365.Sdk
 {
 	/// <summary>
 	/// Preauth settings
 	/// </summary>
+	[DataContract(Namespace = "http://schemas.target365.io/core")]
 	public class PreAuthSettings
 	{
 		/// <summary>
 		/// Info message sent before preauth message
 		/// </summary>
+		[DataMember]
 		public string InfoText { get; set; }
 
 		/// <summary>
 		/// Sender of info message
 		/// </summary>
+		[DataMember]
 		public string InfoSender { get; set; }
 
 		/// <summary>
 		/// Text inserted before preauth text
 		/// </summary>
+		[DataMember]
 		public string PrefixMessage { get; set; }
 
 		/// <summary>
 		/// Text inserted after preauth text
 		/// </summary>
+		[DataMember]
 		public string PostfixMessage { get; set; }
 
 		/// <summary>
 		/// Delay in minutes between info message and preauth message
 		/// </summary>
+		[DataMember]
 		public decimal? Delay { get; set; }
 
 		/// <summary>
 		/// MerchantId to perform preauth on
 		/// </summary>
+		[DataMember]
 		public string MerchantId { get; set; }
 
 		/// <summary>
 		/// Service description for Strex "Min Side"
 		/// </summary>
+		[DataMember]
 		public string ServiceDescription { get; set; }
-		
+
 		/// <summary>
 		/// If settings are active
 		/// </summary>
+		[DataMember]
 		public bool? Active { get; set; }
 	}
 }
