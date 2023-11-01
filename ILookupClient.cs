0b17dc8a217a87f1e382e2f806c9c2566c123d6c
hans.olav@target365.no <hans.olav@target365.no>
2023-11-01 16:40:54 +0100

    Added missing method in ILookupClient interface.

--- ILookupClient.cs
+++ ILookupClient.cs
@@ -14,5 +14,12 @@ namespace Target365.Sdk
 		/// <param name="msisdn">Mobile phone number. Format is full international msisdn including a leading plus, ex: '+4798079008'.</param>
 		/// <param name="cancellationToken">Cancellation token.</param>
 		Task<Lookup> LookupAsync(string msisdn, CancellationToken cancellationToken = default);
+
+		/// <summary>
+		/// Looks up address info from free text (name, address...).
+		/// </summary>
+		/// <param name="freeText">Free text like name or address.</param>
+		/// <param name="cancellationToken">Cancellation token.</param>
+		Task<Lookup[]> LookupFreeTextAsync(string freeText, CancellationToken cancellationToken = default);
 	}
 }
