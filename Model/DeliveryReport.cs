5e3ede7e534c8d63b137d60be770a18a725a7267
Hans Olav Stjernholm <hans.olav@target365.no>
2024-01-12 13:20:27 +0100

    Added DeliveryReport StatusDescription and SmscStatus properties.

--- Model/DeliveryReport.cs
+++ Model/DeliveryReport.cs
@@ -4,100 +4,112 @@ using System.Runtime.Serialization;
 
 namespace Target365.Sdk
 {
-  /// <summary>
-  /// Delivery report.
-  /// </summary>
-  [DataContract(Namespace = "http://schemas.target365.io/core")]
-  public class DeliveryReport
-  {
 	/// <summary>
-	/// Transaction id associated with the message.
+	/// Delivery report.
 	/// </summary>
-	[DataMember]
-	public string TransactionId { get; set; }
+	[DataContract(Namespace = "http://schemas.target365.io/core")]
+	public class DeliveryReport
+	{
+		/// <summary>
+		/// Transaction id associated with the message.
+		/// </summary>
+		[DataMember]
+		public string TransactionId { get; set; }
 
-	/// <summary>
-	/// Correlation id associated with the message.
-	/// </summary>
-	[DataMember]
-	public string CorrelationId { get; set; }
+		/// <summary>
+		/// Correlation id associated with the message.
+		/// </summary>
+		[DataMember]
+		public string CorrelationId { get; set; }
 
-	/// <summary>
-	/// Session id associated with the message.
-	/// </summary>
-	[DataMember]
-	public string SessionId { get; set; }
+		/// <summary>
+		/// Session id associated with the message.
+		/// </summary>
+		[DataMember]
+		public string SessionId { get; set; }
 
-	/// <summary>
-	/// Price associated with the message.
-	/// </summary>
-	[DataMember]
-	public decimal? Price { get; set; }
+		/// <summary>
+		/// Price associated with the message.
+		/// </summary>
+		[DataMember]
+		public decimal? Price { get; set; }
 
-	/// <summary>
-	/// Sender associated with the message.
-	/// </summary>
-	[DataMember]
-	public string Sender { get; set; }
+		/// <summary>
+		/// Sender associated with the message.
+		/// </summary>
+		[DataMember]
+		public string Sender { get; set; }
 
-	/// <summary>
-	/// Recipient associated with the message.
-	/// </summary>
-	[DataMember]
-	public string Recipient { get; set; }
+		/// <summary>
+		/// Recipient associated with the message.
+		/// </summary>
+		[DataMember]
+		public string Recipient { get; set; }
 
-	/// <summary>
-	/// Operator id associated with the message. Can be 'no.telenor', 'no.telia', 'no.ice' etc.
-	/// </summary>
-	[DataMember]
-	public string OperatorId { get; set; }
+		/// <summary>
+		/// Operator id associated with the message. Can be 'no.telenor', 'no.telia', 'no.ice' etc.
+		/// </summary>
+		[DataMember]
+		public string OperatorId { get; set; }
 
-	/// <summary>
-	/// Overall status code.
-	/// </summary>
-	[DataMember]
-	public string StatusCode { get; set; }
+		/// <summary>
+		/// Overall status code.
+		/// </summary>
+		[DataMember]
+		public string StatusCode { get; set; }
 
-	/// <summary>
-	/// Detailed status code.
-	/// </summary>
-	[DataMember]
-	public string DetailedStatusCode { get; set; }
+		/// <summary>
+		/// Detailed status code.
+		/// </summary>
+		[DataMember]
+		public string DetailedStatusCode { get; set; }
 
-	/// <summary>
-	/// Whether message was delivered. Null if status is unknown.
-	/// </summary>
-	[DataMember]
-	public bool? Delivered { get; set; }
+		/// <summary>
+		/// Description of status code.
+		/// </summary>
+		[DataMember]
+		public string StatusDescription { get; set; }
 
-	/// <summary>
-	/// Whether billing was performed. Null if status is unknown.
-	/// </summary>
-	[DataMember]
-	public bool? Billed { get; set; }
+		/// <summary>
+		/// Whether message was delivered. Null if status is unknown.
+		/// </summary>
+		[DataMember]
+		public bool? Delivered { get; set; }
 
-	/// <summary>
-	/// SMSC transaction id.
-	/// </summary>
-	[DataMember]
-	public string SmscTransactionId { get; set; }
+		/// <summary>
+		/// Whether billing was performed. Null if status is unknown.
+		/// </summary>
+		[DataMember]
+		public bool? Billed { get; set; }
 
-	/// <summary>
-	/// SMSC message parts.
-	/// </summary>
-	[DataMember]
-	public int SmscMessageParts { get; set; }
+		/// <summary>
+		/// SMSC transaction id.
+		/// </summary>
+		[DataMember]
+		public string SmscTransactionId { get; set; }
 
-	/// <summary>
-	/// Timestamp for when the message was received at the handset (resolution to whole minute, not second).
-	/// </summary>
-	[DataMember]
-	public DateTimeOffset? Received { get; set; }
+		/// <summary>
+		/// SMSC status.
+		/// </summary>
+		[DataMember(EmitDefaultValue = false)]
+		public string SmscStatus { get; set; }
 
-	/// <summary>
-	/// Custom properties associated with the out-message.
-	/// </summary>
-	[DataMember]
-	public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
-  }
+		/// <summary>
+		/// SMSC message parts.
+		/// </summary>
+		[DataMember]
+		public int SmscMessageParts { get; set; }
+
+		/// <summary>
+		/// Timestamp for when the message was received at the handset (resolution to whole minute, not second).
+		/// </summary>
+		[DataMember]
+		public DateTimeOffset? Received { get; set; }
+
+		/// <summary>
+		/// Custom properties associated with the out-message.
+		/// </summary>
+		[DataMember]
+		public IDictionary<string, object> Properties { get; set; } = new Dictionary<string, object>();
+	}
 }
