d1c83c9fc67f5f2e5ff8f24cf08fac1d31583ea9
Hans Olav Stjernholm <hans.olav@target365.no>
2024-11-28 12:26:35 +0100

    Added keyword mode Startwith and deprecated Text.

--- Model/KeywordModes.cs
+++ Model/KeywordModes.cs
@@ -1,14 +1,22 @@
-﻿namespace Target365.Sdk
+﻿using System;
+
+namespace Target365.Sdk
 {
 	/// <summary>
 	/// Keyword modes.
 	/// </summary>
 	public static class KeywordModes
-	{
+	{		
 		/// <summary>
-		/// Text mode with plain text matching.
+		/// Deprecated - use <see cref="Startswith"/> instead.
 		/// </summary>
+		[Obsolete("Text is obsolete - please use Startswith instead", false)]
 		public const string Text = "Text";
+
+		/// <summary>
+		/// Matching start of text.
+		/// </summary>
+		public const string Startswith = "Startswith";
 
 		/// <summary>
 		/// Wildcard mode with support for special characters like '*', '?', '#' etc.
